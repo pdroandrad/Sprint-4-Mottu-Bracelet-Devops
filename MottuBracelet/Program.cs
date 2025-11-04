a9a4f7287a0172c968f80dda89725b891478255d
Pedro Andrade <161619263+pdroandrad@users.noreply.github.com>
2025-11-04 13:27:40 -0300

    Update Program.cs

--- MottuBracelet/Program.cs
+++ MottuBracelet/Program.cs
@@ -5,18 +5,18 @@ using Oracle.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona serviÁos de controller
+// Adiciona servi√ßos de controller
 builder.Services.AddControllers();
 
 // Configura Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configura a conex„o com Oracle
+// Configura a conex√£o com Oracle
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Registra os serviÁos da aplicaÁ„o
+// Registra os servi√ßos da aplica√ß√£o
 builder.Services.AddScoped<ServicoMotos>();
 builder.Services.AddScoped<ServicoPatios>();
 builder.Services.AddScoped<ServicoDispositivos>();
@@ -25,11 +25,9 @@ builder.Services.AddScoped<ServicoHistoricoPatios>();
 var app = builder.Build();
 
 // Pipeline HTTP
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
